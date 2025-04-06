3b836f23e6b5f4ed5635be60f52eed122aa39226
Kasper Pärlklo <kasperparlklo@gmail.com>
2025-04-06 09:39:07 +0200

    Om Skerry text update

--- src/app/om-skerry/page.js
+++ src/app/om-skerry/page.js
@@ -30,14 +30,13 @@ const AboutPage = () => {
             <h3 className="text-xl font-medium mb-3">Hur Skerry arbetar och tänker</h3>
             <div className="space-y-3 text-base">
               <p>
-              Skerry föreläser, utbildar och erbjuder rådgivning inom AI – med målet att hjälpa företag göra AI till en naturlig del av sin kognitiva verktygslåda. Det handlar dels om att utveckla tankemodeller för hur AI kan användas beroende på vad för typ av information man behöver, och förstå vilka AI-verktyg som lämpar sig för olika behov. När AI förstås på det här sättet ökar förmågan att fatta snabbare och mer välgrundade beslut. 
+              Skerry föreläser, utbildar och erbjuder rådgivning inom AI – med målet att hjälpa företag göra AI till en naturlig del av sin kognitiva verktygslåda. Det handlar om att utveckla tankemodeller för hur AI kan användas beroende på vilken information man behöver, och att lära sig vilka verktyg som passar för olika syften. När AI förstås på det här sättet ökar förmågan att fatta snabbare och mer välgrundade beslut. 
               </p>
               <p>
               För företag som börjar bygga kompetens och vana i att arbeta med AI idag, lägger grunden för ett arbetssätt som snart kommer att vara standard. Att skjuta upp adoptionen innebär inte att man undviker förändring – det innebär att man senare tvingas göra det under sämre villkor, i ett klimat där konkurrenter redan vant sig vid en ny hastighet. Skillnaderna börjar redan visa sig – mellan de som aktivt integrerar AI i sitt arbete och de som inte gör det – och resultatet är tydligt: AI skapar utrymme för mer kvalificerat och kreativt arbete. 
               </p>
               <p>
-              Samtidigt håller intelligens på att bli en handelsvara. När det som tidigare definierade kunskapsarbete i allt högre grad kan automatiseras, blir den avgörande frågan hur man differentierar sig genom sitt mänskliga bidrag. Och då gäller det att tänka länge och hårt på vilka förmågor och perspektiv som inte går att replikera genom en maskin - och att förstå och använda AI på ett medvetet sätt är just ett sådant bidrag. Hursomhelst så är det den reflektionen – och hur den omsätts i arbetet – som kommer att skilja bolag åt.
-              </p>
+              Samtidigt håller intelligens på att bli en handelsvara. När det som tidigare definierade kunskapsarbete i allt högre grad kan automatiseras, blir det avgörande att tänka länge och hårt på hur man fortfarande kan differentiera sig genom sitt mänskliga bidrag - och ett sådant bidrag handlar om förmågan att förstå och använda AI. För det är den reflektionen om att förstå vad som inte låter sig replikeras – och hur den omsätts i arbetet – som kommer att skilja bolag åt.              </p>
             </div>
           </section>
 
