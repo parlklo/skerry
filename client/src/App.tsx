59adf72181dab166a0fc68dea156343985c274c2
Kasper Pärlklo <kasperparlklo@gmail.com>
2025-06-12 10:36:08 +0200

    nytt minor fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,7 @@
+import React from "react";
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/toaster";
 import { Navigation } from "@/components/navigation";
 import Home from "@/pages/home";
 import Services from "@/pages/services";
@@ -25,12 +24,10 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <Toaster />
+      <Router />
+    </TooltipProvider>
   );
 }
 
